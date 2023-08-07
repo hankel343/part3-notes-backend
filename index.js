9e4bb9769820a0a4cee1c0c6126785a13e80289e
hhaldin <hankel343@gmail.com>
2023-08-06 21:25:42 -0500

    add static middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const requestLogger = (request, response, next) => {
@@ -15,6 +16,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
   {
@@ -93,7 +96,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
